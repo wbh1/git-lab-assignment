4375ce92c30f961a79f3f68612a1f1948dd8c678
Ryan Heathcote <rheathcote@sharptop.co>
2017-09-15 15:31:53 -0400

    make events images dynamic too.

--- www/src/pages/events/list.js
+++ www/src/pages/events/list.js
@@ -12,18 +12,21 @@ import {inject, bindable, bindingMode} from "aurelia-framework";
 >>>>>>> Changed the way the filtered content works to work with events
 import {EventService, NavigationService} from "../../services/index";
 import {Router} from "aurelia-router";
+import {ConfigurationHolder} from "../../resources/configuration-holder";
 
-@inject(EventService, Router, NavigationService)
+@inject(EventService, Router, NavigationService, ConfigurationHolder)
 export class EventsList {
 
     @bindable ({defaultBindingMode: bindingMode.twoWay}) filteredEvents = []
 
-    constructor(eventService, router, navigationService) {
+    constructor(eventService, router, navigationService, configurationHolder) {
         this.eventService = eventService
         this.router = router
         this.navigationService = navigationService
         this.events = eventService.list()
         this.events.promise.then((events) => this.filteredEvents = events)
+
+        this.eventsImageURL = configurationHolder.get('eventsImageURL')
     }
 
     show(event) {

f6d0567655373afdd8873a5da5cb61f63d2a3a79
Ryan Heathcote <rheathcote@sharptop.co>
2017-09-15 12:08:50 -0400

    Fix prayer requests

--- www/src/pages/prayer-requests/list.js
+++ www/src/pages/prayer-requests/list.js
@@ -6,7 +6,14 @@
  */
 
 import {inject} from 'aurelia-framework'
+<<<<<<< HEAD
 import {PrayerRequestService, NavigationService} from '../../services/index'
+=======
+import {PrayerRequestService, NavigationService, MessageService} from 'services/index'
+import {Router} from "aurelia-router";
+import {PagedContentResolver} from "resources/templates/paged-content/paged-content-resolver"
+import {PagedContentMemory} from "resources/templates/paged-content/paged-content-memory"
+>>>>>>> Fix prayer requests
 
 @inject(PrayerRequestService, NavigationService)
 export class PrayerRequestsList {

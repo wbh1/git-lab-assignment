3b028fddad107f9a08420bda35f449a237dd1e98
Ryan Heathcote <rheathcote@sharptop.co>
2017-09-15 15:07:21 -0400

    Move the PrayerRequest helper class content into the models.

--- www/src/models/prayer-request.js
+++ www/src/models/prayer-request.js
@@ -7,6 +7,11 @@
 
 export class PrayerRequest {
 
+    title = ""
+    author = ""
+    description = ""
+    secret = true
+
     static domainClass = 'co.sharptop.church.PrayerRequest'
     static endpoint = 'prayer-requests'
 }

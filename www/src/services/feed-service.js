2cd82384537fb75e2abafd7f7df30a5a1254c527
Ryan Heathcote <rheathcote@sharptop.co>
2017-09-18 17:00:53 -0400

    Update the feed when the feed cache gets refreshed.

--- www/src/services/feed-service.js
+++ www/src/services/feed-service.js
@@ -6,15 +6,61 @@
  */
 
 import {inject} from "aurelia-framework";
+import {EventAggregator} from "aurelia-event-aggregator";
 import {Endpoint} from "aurelia-api";
 import {ResourceService} from "./resource-service";
 import {Feed} from "models/index"
 
+<<<<<<< HEAD
 @inject(Endpoint.of('api'))
 export class FeedService extends ResourceService {
 
     constructor(api) {
         super(api, Feed)
+=======
+@inject(Endpoint.of('api'), ObjectCache, EventAggregator)
+export class FeedService extends ResourceService {
+
+    lastUpdated
+
+    constructor(api, objectCache, EventAggregator) {
+        super(api, Feed)
+        this.objectCache = objectCache
+        this.eventAggregator = EventAggregator
+
+        setInterval(() => this.refreshCache(), 60000)
+    }
+
+    findOne(id) {
+        if (this.objectCache.has(Feed.domainClass)) {
+            return this.objectCache.get(Feed.domainClass)
+        }
+
+        let feed = super.findOne(id)
+
+        this.cacheFeed(feed)
+
+        return feed
+    }
+
+
+    cacheFeed(feed) {
+        feed.promise.then((res) => {
+            console.log("Updating Feed Cache")
+            this.objectCache.set(Feed.domainClass, res)
+            this.objectCache.traverse(res)
+            this.lastUpdated = Date.now()
+            this.eventAggregator.publish("feed.cache.updated")
+        })
+    }
+
+    refreshCache() {
+        console.log("Checking Feed Cache for freshness")
+        if (Date.now() - this.lastUpdated > 30000) {
+            console.log("Refreshing Feed")
+            this.cacheFeed(super.findOne())
+        }
+>>>>>>> Update the feed when the feed cache gets refreshed.
     }
 
 }
